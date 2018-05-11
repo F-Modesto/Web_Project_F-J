109d13d8a9e7626d3c9e43c59f395b7c82480e18
Joao Bras Monteiro <joao.rbmonteiro1@gmail.com>
2018-05-11 15:54:52 +0100

    kkk
    dasd

--- server.js
+++ server.js
@@ -1,7 +1,49 @@
+var fs = require('fs');
+var data = fs.readFileSync('volunteeringData.json');
+
+console.log(data);
+
+var volunteering = JSON.parse(data);
+
+console.log(volunteering);
+
 const express = require('express');
 const app = express();
 
-app.use(express.static("web"));
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://JoaoBM:<JoaoBM123>@cluster0-anqkh.mongodb.net/test?retryWrites=true";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
+// app.use(express.static('web'));
+
+// app.get('/all', sendResp);
+
+// function sendResp(req,res) {
+// 	res.send(volunteering);
+// }
+
+// app.get('/add/:entryName/:entryEmail/', addEntry);
+
+// function addEntry(req,res) {
+
+// 	var reqData = req.params;
+// 	var name = reqData.entryName;
+// 	var email = reqData.entryEmail;
+
+// 	volunteering[name] = email;
+
+// 	var data = JSON.stringify(volunteering, null, 2);
+
+// 	fs.writeFile('volunteeringData.json', data, finished);
+// 	function finished() {
+
+// 		console.log('File Saved.');
+// 	}
+// }
---
+++ web/script.js
@@ -0,0 +1,34 @@
+function getData() {
+
+	$.getJSON('/all', show);
+
+	function show(data) {
+
+	var key = Object.keys(data);
+	console.log(key);
+	var name = key[0];
+	var email = data[name];
+	console.log(name);
+	console.log(email);
+	document.getElementById('entry').innerHTML = name + " " + email;
+	}
+}
+
+getData();
+
+var but = document.getElementById('submit');
+
+but.onclick = function send() {
+
+	var entryN = document.getElementById('name').value;
+	var entryE = document.getElementById('email').value;
+
+	console.log(entryN);
+	console.log(entryE);
+
+	$.getJSON('add/' + entryN + '/' + entryE, end);
+
+	function end(data) {
+	  console.log(data);
+	}
+}
