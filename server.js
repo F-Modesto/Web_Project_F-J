59209957f38b9a2a7aeb2e1ac77645e96a296832
Joao Bras Monteiro <joao.rbmonteiro1@gmail.com>
2018-05-16 15:32:22 +0100

    .

--- server.js
+++ server.js
@@ -1,49 +1,139 @@
-var fs = require('fs');
-var data = fs.readFileSync('volunteeringData.json');
-
-console.log(data);
-
-var volunteering = JSON.parse(data);
-
-console.log(volunteering);
-
 const express = require('express');
 const app = express();
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://JoaoBM:<JoaoBM123>@cluster0-anqkh.mongodb.net/test?retryWrites=true";
+var url = "mongodb+srv://Admin:Admin123@cluster0-anqkh.mongodb.net/test?retryWrites=false";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
+//CONNECTION
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   console.log("Connection to DataBase successfuly established!");
+//   db.close();
+// });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// //CREATE COLLECTION
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("H2Oil");
+//   dbo.createCollection("customers", function(err, res) {
+//     if (err) throw err;
+//     console.log("Collection created!");
+//     db.close();
+//   });
+// });
 
-// app.use(express.static('web'));
+// //INSERT
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("H2Oil");
+//   var myobj = { name: "Company Inc", address: "Highway 37" };
+//   dbo.collection("customers").insertOne(myobj, function(err, res) {
+//     if (err) throw err;
+//     console.log("1 document inserted");
+//     db.close();
+//   });
+// });
 
-// app.get('/all', sendResp);
+// //FIND
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("H2Oil");
+//   dbo.collection("customers").findOne({}, function(err, result) {
+//     if (err) throw err;
+//     console.log(result.name);
+//     db.close();
+//   });
+// });
 
-// function sendResp(req,res) {
-// 	res.send(volunteering);
-// }
+// //QUERY
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("H2Oil");
+//   var query = { address: "Highway 37" };
+//   dbo.collection("customers").find(query).toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     db.close();
+//   });
+// });
 
-// app.get('/add/:entryName/:entryEmail/', addEntry);
+// //SORT
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("H2Oil");
+//   var mysort = { name: 1 };
+//   dbo.collection("customers").find().sort(mysort).toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     db.close();
+//   });
+// });
 
-// function addEntry(req,res) {
+// //DELETE
+// // MongoClient.connect(url, function(err, db) {
+// //   if (err) throw err;
+// //   var dbo = db.db("H2Oil");
+// //   var myquery = { address: 'Mountain 21' };
+// //   dbo.collection("customers").deleteOne(myquery, function(err, obj) {
+// //     if (err) throw err;
+// //     console.log("1 document deleted");
+// //     db.close();
+// //   });
+// // });
 
-// 	var reqData = req.params;
-// 	var name = reqData.entryName;
-// 	var email = reqData.entryEmail;
+// //DROP COLLECTION
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("H2Oil");
+//   dbo.collection("customers").drop(function(err, delOK) {
+//     if (err) throw err;
+//     if (delOK) console.log("Collection deleted");
+//     db.close();
+//   });
+// });
 
-// 	volunteering[name] = email;
+// //UPDATE
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("H2Oil");
+//   var myquery = { address: "Valley 345" };
+//   var newvalues = { $set: {name: "Mickey", address: "Canyon 123" } };
+//   dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+//     if (err) throw err;
+//     console.log("1 document updated");
+//     db.close();
+//   });
+// }); 
 
-// 	var data = JSON.stringify(volunteering, null, 2);
+// //LIMIT
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("H2Oil");
+//   dbo.collection("customers").find().limit(5).toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     db.close();
+//   });
+// });
 
-// 	fs.writeFile('volunteeringData.json', data, finished);
-// 	function finished() {
+// //JOIN
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("H2Oil");
+//   dbo.collection('orders').aggregate([
+//     { $lookup:
+//        {
+//          from: 'products',
+//          localField: 'product_id',
+//          foreignField: '_id',
+//          as: 'orderdetails'
+//        }
+//      }
+//     ]).toArray(function(err, res) {
+//     if (err) throw err;
+//     console.log(JSON.stringify(res));
+//     db.close();
+//   });
+// });
 
-// 		console.log('File Saved.');
-// 	}
-// }
+app.listen(3000, () => console.log('Connection available on port 3000'));
