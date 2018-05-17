7195243610e6cc784477150695e36b2c0a32d46d
Francisco Modesto <34067686+F-Modesto@users.noreply.github.com>
2018-05-17 17:25:32 +0100

    minor changes

--- server.js
+++ server.js
@@ -24,13 +24,14 @@ app.post("/addname", (req, res) => {
     var myData = new User(req.body);
     myData.save()
         .then(item => {
-            res.send("Name saved to database");
+           console.log("Name saved to database");
         })
         .catch(err => {
-            res.status(400).send("Unable to save to database");
+            console.log(400).send("Unable to save to database");
         });
 });
 
 app.listen(port, () => {
     console.log("Server listening on port " + port);
 });
+
